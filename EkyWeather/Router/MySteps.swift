95e20f57189274a437608ac1988bc0dd7f9b2a54
rizkyeky <mochamad.rizky.darmawan>
2025-07-11 10:35:22 +0700

    fix code #1

--- EkyWeather/Router/MySteps.swift
+++ EkyWeather/Router/MySteps.swift
@@ -5,28 +5,28 @@
 //  Created by Eky on 30/01/25.
 //
 
-import RouteComposer
-import UIKit
+// import RouteComposer
+// import UIKit
 
-// Define steps for navigation
-struct MySteps {
-    static let home = StepAssembly(
-        finder: HomeVCFinder(),
-        factory: HomeVCFactory()
-    )
-        .using(UINavigationController.push())
-        .from(NavigationControllerStep())
-        .using(GeneralAction.nilAction())
-        .from(GeneralStep.root())
-        .assemble()
+// // Define steps for navigation
+// struct MySteps {
+//     static let home = StepAssembly(
+//         finder: HomeVCFinder(),
+//         factory: HomeVCFactory()
+//     )
+//         .using(UINavigationController.push())
+//         .from(NavigationControllerStep())
+//         .using(GeneralAction.nilAction())
+//         .from(GeneralStep.root())
+//         .assemble()
 
-    static let login = StepAssembly(
-        finder: ClassFinder<LoginViewController, Void>(),
-        factory: LoginVCFactory()
-    )
-    .using(UINavigationController.push())
-    .from(NavigationControllerStep())
-    .using(GeneralAction.nilAction())
-    .from(GeneralStep.root())
-    .assemble()
-}
+//     static let login = StepAssembly(
+//         finder: ClassFinder<LoginViewController, Void>(),
+//         factory: LoginVCFactory()
+//     )
+//     .using(UINavigationController.push())
+//     .from(NavigationControllerStep())
+//     .using(GeneralAction.nilAction())
+//     .from(GeneralStep.root())
+//     .assemble()
+// }
